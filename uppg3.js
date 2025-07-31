068229a668f88f47f556e3f0e7d904727874ba78
BisherS <bisher.2000.se@gmail.com>
2025-07-31 23:32:31 +0800

    uppgift 3,4 and 5 are done

--- uppg3.js
+++ uppg3.js
@@ -7,6 +7,13 @@ function uppg3() {
     // tilldela värdena 5 och 10 till variablerna
     // skriv ut summan av a och b i konsolen
 
+    // deklarera två variabler, a och b
+    const a = 5;
+    const b = 10;
+
+    // skriv ut summan av a och b i konsolen
+    const summa = a + b;
+    console.log(summa);
 
 
 }
--- uppg5.js
+++ uppg5.js
@@ -4,6 +4,14 @@ function uppg5() {
     // om det första talet är större än det andra, skriv ut "Det första talet är större än det andra"
     // annars skriv ut "Det andra talet är större än det första"
 
+    const tal1 = 10;
+    const tal2 = 7;
+
+    if (tal1 > tal2) {
+        console.log("Det första talet är större än det andra");
+    } else {
+        console.log("Det andra talet är större än det första");
+    }
 
 
 }
